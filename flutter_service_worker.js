23edaa3521d6ece0026b85bd3c570bf6706f2d99
Iurii Dorofeev <otopba@inbox.ru>
2020-11-18 01:42:54 +0300

    last 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "22071543f40d4cbb8fd49b0152d6edce",
-"index.html": "1521e96a5d49e051fa829352411006d0",
-"/": "1521e96a5d49e051fa829352411006d0",
-"main.dart.js": "f1e739e894c22479d0119d5e52b5f7dc",
+"index.html": "8d04629e93d9b7500196fccce49c4fba",
+"/": "8d04629e93d9b7500196fccce49c4fba",
+"main.dart.js": "4c303d9f8a4e80f309305a4315f2e07b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
